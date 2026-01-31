9586f9ebb701783f1de00e60ed443c3b5cc6aa9f
Dércio Bobo <38473260+DercioBobo@users.noreply.github.com>
2026-01-31 19:49:50 +0200

    Version 2 - Send files missing file

---
+++ whatsapp_notifications/whatsapp_notifications/doctype/whatsapp_media_doctype/whatsapp_media_doctype.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024, Entretech and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WhatsAppMediaDocType(Document):
+    # This is a child table, no additional logic needed
+    pass
